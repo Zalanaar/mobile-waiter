db1c782e0a353580fe6438acdf176eb0cab6b3c5
Unknown <1cheaterok1@gmail.com>
2017-10-17 15:54:27 +0300

    Создаёт базовое Android-приложение #MW-12

---
+++ src/mobile/app/src/main/java/zalanaar/github/com/mobilewaiter/MainActivity.java
@@ -0,0 +1,13 @@
+package zalanaar.github.com.mobilewaiter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
