25046452551fb612fb94ea3ac874923d3e7c5613
Vsevolod <1cheaterok1@gmail.com>
2017-12-27 10:53:27 +0300

    Добавляет логику ЛК

--- src/backend/db_functions.py
+++ src/backend/db_functions.py
@@ -1,6 +1,12 @@
 import pyrebase
 
 
+class DBException(Exception): pass
+
+
+class PhoneExists(DBException): pass
+
+
 class Connection:
 
     def __init__(self, config):
@@ -14,22 +20,39 @@ class Connection:
         # Непосредственно подключение к БД
         self.db = self.firebase.database()
 
+    def _get_logins(self):
+        return dict(self.db.child("User_rest").get().val())
+
     def is_login(self, phone, password) -> True or False:
-        pass
+        try:
+            return self._get_logins()[phone]['Password'] == password
+        except KeyError:
+            return False
 
     def is_phone_exists(self, phone) -> True or False:
-        pass
+        return phone in self._get_logins()
 
     def sign_up(self, phone, name, password) -> None:
-        pass
+        if self.is_phone_exists(phone):
+            raise PhoneExists(phone)
+        self.db.child("User_rest").child(phone).set({"Name": name, "Password": password})
 
     def get_dishes(self, phone) -> [dict]:
-        pass
+        rest_id = list(dict(self.db.child("Restaurant").order_by_child("User_restId").equal_to(phone).get().val()).keys())[0]
+        categories = list(self.db.child("Category").order_by_child("RestId").equal_to(rest_id).get().val().keys())
+        dish_categories = []
+        for category in categories:
+            try:
+                dish_categories += self.db.child("Food").order_by_child("MenuID").equal_to(category).get().val()
+            except IndexError:
+                pass
+
+        return [dict(self.db.child("Food").child(dish_category).get().val()) for dish_category in dish_categories]
 
     def add_dish(self, phone, info) -> None:
         pass
 
-    def add_category(self, phone, name) -> None:
+    def add_category(self, phone, name, image) -> None:
         pass
 
 
@@ -43,4 +66,9 @@ config = {
 CONNECTION = Connection(config)
 
 
-
+if __name__ == '__main__':
+    print(CONNECTION.is_login("6666", "123"))
+    print(CONNECTION.is_phone_exists("7777"))
+    # CONNECTION.sign_up("2722", "Andrey", "Lox")
+    for dish in CONNECTION.get_dishes("6666"):
+        print(dish)
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,14 @@
+from collections import OrderedDict
+
 from flask import *
+
+from db_functions import CONNECTION
 
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
-FOOD_INFO = "Название Описание Цена Картинка Категория".split()
+FOOD_INFO = "Название Описание Картинка Категория Цена Скидка".split()
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -12,26 +16,40 @@ def index():
     if request.method == 'POST':
         form = request.form
         if "Sign In" in form:
-            if True:
-                session['user'] = form['phone']
+            print(form['phone'], form['pass'])
+            if CONNECTION.is_login(form['phone'], form['pass']):
+                session['phone'] = form['phone']
                 return redirect(url_for("account"))
         elif "Sign Up" in form:
-            print(form["email"], form["phone"])
-            if True:
+            if not CONNECTION.is_phone_exists(form['phone']):
+                CONNECTION.sign_up(form['phone'], form['name'], form['pass'])
+                session['phone'] = form['phone']
                 return redirect(url_for("account"))
     return render_template('index.html')
 
 
 @app.route("/account/", methods=['GET', 'POST'])
 def account():
-    user = session['user']
+    user = session['phone']
     if request.method == "POST":
-        print(*request.form.items())
-    # client = {
-    #     'name': 'kek',
-    #     'tel': 'kek',
-    # }
-    items = [['kek', 'kek', 'kek', 'kek', 'kek'],
-             ['shpek', 'shpek', 'shpek', 'shpek', 'shpek']]
+        form = request.form
+        image = form['image']
+        name = form['name']
+        CONNECTION.add_category(user, name, image)
 
-    return render_template('account.html', info=FOOD_INFO, items=items)
+    else:
+        try:
+            items = [item for item in CONNECTION.get_dishes(session['phone'])]
+            items_ordered = []
+            print(items)
+
+            for item in items:
+                kek = [item['Name'], item['Description'], item['Image'], item['MenuID'], item['Price'], item['Discount'],]
+                items_ordered.append(kek)
+
+            print(items_ordered)
+
+        except IndexError:
+            items_ordered = []
+
+    return render_template('account.html', info=FOOD_INFO, items=items_ordered)
